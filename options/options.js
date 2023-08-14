e2e1c90c48939c8f50145cbc7ec60b2d5f3d9fdc
Crowdbotics <engineering@crowdbotics.com>
2023-08-14 23:24:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-bird-dev-109187",
+  url: "https://fancy-bird-dev-109187.botics.co",
+  api: "https://fancy-bird-dev-109187.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
